2dcb9fa5c1d6dfd3cbd3b8dd786606b467947130
Arroyojava <94626607+Arroyojava@users.noreply.github.com>
2023-08-17 15:58:50 -0400

    Added labels and buttons

--- main.py
+++ main.py
@@ -17,15 +17,31 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window Config
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Canvas / Background
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+# Label
+timer_label = Label(text='Timer', font=(FONT_NAME, 50, 'bold'), bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+check_label = Label(text='✔', font=(FONT_NAME, 20), bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
+
+# Buttons
+start_button = Button(text='Start', font=(FONT_NAME, 12), bg=YELLOW, fg=RED)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text='Reset', font=(FONT_NAME, 12), bg=YELLOW, fg=RED)
+reset_button.grid(column=2, row=2)
 
 window.eval('tk::PlaceWindow . Center')
 window.mainloop()
